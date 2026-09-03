6395b2e3a74d6d3af620a2259b6d40a89c750cc2
genesiszaim-png <genesiszaim@gmail.com>
2026-09-03 12:20:06 +0800

    Created using Colab

---
+++ testing_py.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "name": "TESTING.PY",
+      "authorship_tag": "ABX9TyNnji1YUVLCldaF8p/K/W6P",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/genesiszaim-png/chatbot/blob/main/testing_py.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "id": "Qw5c5LV4e8a6",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "outputId": "3c6d5842-bd66-437a-f02f-fb233d8d73a4"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hello\n"
+          ]
+        }
+      ],
+      "source": [
+        "print (\"hello\")"
+      ]
+    }
+  ]
+}
